2400c041d99c459151400bec3bd6e6711950e2ee
Nikita1600 <125687366+Nikita1600@users.noreply.github.com>
2023-04-01 17:07:59 +0530

    inserting num in linked list

--- Program.cs
+++ Program.cs
@@ -1,4 +1,6 @@
-﻿namespace Linkedlist
+﻿using System.Collections.Generic;
+
+namespace Linkedlist
 {
     class Program
     {
@@ -6,6 +8,12 @@
         public static void Main(String[] args)
         {
             Console.WriteLine("Welcom to the Linked List Program");
+
+            linkedlist linkedList = new linkedlist();
+            linkedList.Add(56);
+            linkedList.Add(30);
+            linkedList.Add(70);
+            linkedList.Display();
         }
     }
 }
