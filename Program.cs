becde26d2ee9a033b988a02fe160dd718452dd5b
Nikita1600 <125687366+Nikita1600@users.noreply.github.com>
2023-04-01 16:24:04 +0530

    Welcome Page

---
+++ Program.cs
@@ -0,0 +1,11 @@
+﻿namespace Linkedlist
+{
+    class Program
+    {
+
+        public static void Main(String[] args)
+        {
+            Console.WriteLine("Welcom to the Linked List Program");
+        }
+    }
+}
