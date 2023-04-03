de385eac2895abbbfc8dc223b7cfc67c6827e031
Nikita1600 <125687366+Nikita1600@users.noreply.github.com>
2023-04-03 19:03:21 +0530

    inserting a value nd add new node

--- Program.cs
+++ Program.cs
@@ -8,13 +8,17 @@ namespace Linkedlist
         public static void Main(String[] args)
         {
             Console.WriteLine("Welcom to the Linked List Program");
-
             linkedlist linkedList = new linkedlist();
             linkedList.Add(56);
             linkedList.Add(30);
             linkedList.Add(70);
             linkedList.Display();
             linkedList.Search(30);
+            linkedList.InsertNode(3, 40);
+            Console.WriteLine("***************************");
+
+            linkedList.Display();
+            Console.WriteLine("40 inserted after 30");
         }
     }
     
