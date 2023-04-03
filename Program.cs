402253220df08e43d9137888b0520b3fac15bd5a
Nikita1600 <125687366+Nikita1600@users.noreply.github.com>
2023-04-03 15:25:33 +0530

    inserting a value

--- Program.cs
+++ Program.cs
@@ -10,10 +10,19 @@ namespace Linkedlist
             Console.WriteLine("Welcom to the Linked List Program");
 
             linkedlist linkedList = new linkedlist();
-            linkedList.Add(56);
-            linkedList.Add(30);
             linkedList.Add(70);
-            linkedList.Display();
+            Console.WriteLine("Node with data 70 is First created ");
+            Console.WriteLine("------------------------------------ ");
+
+            linkedList.Add(30);
+            Console.WriteLine("Next 30 is added to 70 ");
+            Console.WriteLine("------------------------------------ ");
+
+            linkedList.Add(56);
+            Console.WriteLine("Finally56 is added to 30");
+            Console.WriteLine("------------------------------------ ");
+
+            Console.WriteLine("LinkedList Sequence: 56->30->70");
         }
     }
 }
